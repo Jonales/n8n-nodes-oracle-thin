80ef2cf3680642572ab5c821944e1843b19ce0ce
Jônatas Meireles <jonales@gmail.com>
2025-08-18 12:06:28 -0300

    Delete index.ts

--- index.ts
+++
@@ -1,5 +0,0 @@
-// index.ts
-export * from './credentials/Oracle.credentials';
-export * from './nodes/Oracle/OracleDatabaseAdvanced.node';
-export * from './nodes/Oracle/PGVectorStore.node';
-export * from './nodes/Oracle/ChatMemory.node';
